22a1093a54932e3a75a2475d2a8ba5b5e1e5bf47
Jennifer <lopezgomezjennifer50@hotmail.com>
2025-10-08 21:36:49 -0500

    HU05: admin analytics filters (date, category, demography) + CSV export + tests

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    GENDER_CHOICES = (("male","Male"),("female","Female"),("other","Other"))
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+    country = models.CharField(max_length=80, blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Profile {self.user.username}"
