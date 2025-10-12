b4835e75de519cab3c11c3427595e7c4bc4c39cd
Jennifer <lopezgomezjennifer50@hotmail.com>
2025-10-12 16:40:05 -0500

    correcciones

--- Buy4U_Project/settings.py
+++ Buy4U_Project/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret")
 
@@ -155,5 +156,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-load_dotenv()
+
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
